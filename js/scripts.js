97bf03e57aad8c148c49817304166004dc909506
Ryan Heupler <Rheupler@gmail.com>
2015-09-11 07:58:12 -0700

    passing spec not allowing user to enter empty string

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var findAndReplace = function(string, oldWord, newWord) {
+  if (string.length === 0) {
+    return "Please enter a sentence.";
+  };
+};
--- specs/specs.js
+++ specs/specs.js
@@ -1,3 +1,5 @@
 describe('findAndReplace', function() {
-
+  it('prompts user if entered an empty string', function() {
+    expect(findAndReplace("")).to.eq("Please enter a sentence.");
+  });
 });
