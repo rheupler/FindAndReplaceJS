b55f33ee11f9090250679d50d0ae158159a7666c
Ryan Heupler <Rheupler@gmail.com>
2015-09-11 08:01:43 -0700

    specs for replacing old word with updated word

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var findAndReplace = function(string, oldWord, newWord) {
   if (string.length === 0) {
     return "Please enter a sentence.";
-  };
+  } else if (string.search(oldWord >= 0)) {
+    return string.replace(oldWord, newWord);
+  } else {
+    return "Your word was not found."
+  }
 };
