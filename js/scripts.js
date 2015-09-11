17a73c337397df6901936f92a9616145aaa9fee9
Ryan Heupler <Rheupler@gmail.com>
2015-09-11 10:13:52 -0700

    adds html page for showing results

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,38 @@
 var findAndReplace = function(string, oldWord, newWord) {
   if (string.length === 0) {
     return "Please enter a sentence.";
-  } else if (string.search(oldWord >= 0)) {
+  } else if (string.search(oldWord > 0)) {
     return string.replace(oldWord, newWord);
   } else {
-    return "Your word was not found."
+    return "Your word was not found.";
   }
 };
+
+
+// $(document).ready(function() {
+//   $("form#findReplace").submit(function(event) {
+//     var phrase = $("#phrase").val();
+//     var oldWord = $("#oldWord").val();
+//     var newWord = $("#newWord").val();
+//     var result = findReplace(phrase, oldWord, newWord);
+//
+//     $("#results p").text(result);
+//     $("#results").show();
+//
+//     event.preventDefault();
+//   })
+// })
+
+
+
+$(function() {
+  $("form#findReplace").submit(function(event) {
+    var sentence = $("input#string").val();
+    var oldWord = $("input#oldWord").val();
+    var newWord = $("input#newWord").val();
+    var result = findAndReplace(sentence, oldWord, newWord);
+
+    $("#result").text(result);
+    event.preventDefault();
+  });
+});
